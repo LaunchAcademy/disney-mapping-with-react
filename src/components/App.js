edcd1f435f8ebab85445fb041b867d41877baffa
dovakeen118 <kegillis118@gmail.com>
2023-10-04 12:11:04 -0400

    finished code

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,54 @@
 import React from "react"
 
+import MovieTile from "./MovieTile";
+
 const App = props => {
-  let handleClick = clickedMovieId => {
-    let specificSongs = props.data.songs.filter(song => song.movieId === clickedMovieId)
+  const handleClick = clickedMovieId => {
+    // debugger
+    
+    const specificSongs = props.data.songs.filter(song => {
+      // debugger
+      if (song.movieId === clickedMovieId) {
+        return song
+      }
+      // return song.movieId === clickedMovieId
+    })
+
+    // debugger
+    
     let alertMessage = `The songs for this movie are:\n`
     specificSongs.forEach(song => {
       alertMessage += `${song.title}\n`
     })
+    
+    // debugger
     alert(alertMessage)
   }
+  
+  // debugger
+
+  const moviesArray = props.data.movies
+
+  const movieTileArray = moviesArray.map((movie) => {
+    // <li>{movie.title}</li>
+    return (
+      <MovieTile 
+        title={movie.title}
+        id={movie.id}
+        key={movie.id}
+        handleClick={handleClick}
+      />
+    )
+  })
+
 
   return(
     <div className="movie-container">
       <h1>Movies to Watch</h1>
+
+      <ul>
+        {movieTileArray}
+      </ul>
     </div>
   );
 };
--- src/components/MovieTile.js
+++ src/components/MovieTile.js
@@ -1,10 +1,17 @@
 import React from "react"
 
 const MovieTile = (props) => {
+  // console.log(props);
+  
+  const clickButton = () => {
+    console.log("button was clicked!");
+    props.handleClick(props.id)
+  }
+
   return(
     <li>
       {props.title}
-      <button type="button">See Songs for This Movie</button>
+      <button type="button" onClick={clickButton}>See Songs for This Movie</button>
     </li>
   )
 }
